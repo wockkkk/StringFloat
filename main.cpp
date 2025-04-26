1a594d58fee1343326ad204417139717e3c62d73
Administrator <@>
2025-04-26 10:19:13 +0800

    v0.1-format

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ int main() {
     std::string string1;
     std::cout << "in!:";
     std::cin >> string1;
-    StrF str(string1);
+    StrF::StrF str(string1);
     std::cout << "str1 = " << str << endl;
     std::string string2;
     std::cout << "in!:\n";
     std::cin >> string2;
-    StrF str2(string2);
+    StrF::StrF str2(string2);
     std::cout << "str2 = " << str2 << endl;
     str+=str2;
     std::cout << "plus str1=" << str << endl;
